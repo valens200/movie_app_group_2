e7302670426b33abbfb923d56022aa71f58deed6
Hallcoder <hallcoder25@gmail.com>
2022-05-22 19:55:23 +0200

    cloudinary initialized

--- backend/controlllers/moviesController.js
+++ backend/controlllers/moviesController.js
@@ -1,10 +1,17 @@
-const { Movie } = require("../models/movie")
+const { Movie } = require("../models/movie");
+const cloudinary  = require("../utils/cloudinary");
 
-module.exports.createMovie = async() => {
-    return (req,res) => {
+module.exports.createMovie = async(req,res) => {
+        console.log(req.body);
         const movie = new Movie({
-            title:req.body.title,
-            
+            title:req.body.video.name,            
         })
-    }
+        const uploadedMovie = await  cloudinary.uploader.upload(req.body.video,{
+            folder:"movies",
+            public_id:movie.title,
+            use_filename:true
+        })
+        console.log(uploadedMovie);
+        res.send(uploadedMovie);
+    
 }
